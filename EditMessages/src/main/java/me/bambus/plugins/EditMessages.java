a7678b2c0235762e22a8fd22e3a67475acaf3322
HypedDomi <domi@bambus.me>
2021-11-16 23:43:58 +0100

    Update EditMessages.java

--- EditMessages/src/main/java/me/bambus/plugins/EditMessages.java
+++ EditMessages/src/main/java/me/bambus/plugins/EditMessages.java
@@ -2,11 +2,20 @@ package me.bambus.plugins;
 
 import android.content.Context;
 import android.view.View;
+import android.view.Gravity;
 import android.widget.LinearLayout;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.text.InputType;
+import android.text.TextWatcher;
+import android.text.Editable;
+import android.graphics.Color;
 import androidx.core.widget.NestedScrollView;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+import com.aliucord.Constants;
+import android.text.SpannableStringBuilder;
 import com.lytefast.flexinput.R;
 import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.entities.Plugin;
@@ -16,6 +25,7 @@ import com.aliucord.views.Button;
 import com.aliucord.views.DangerButton;
 import com.aliucord.Utils;
 import com.aliucord.utils.DimenUtils;
+import com.aliucord.utils.MDUtils;
 import com.aliucord.PluginManager;
 import com.aliucord.api.CommandsAPI;
 import com.discord.api.commands.ApplicationCommandType;
@@ -49,30 +59,48 @@ public class EditMessages extends Plugin {
             
             var ctx = view.getContext();
             LinearLayout layout = getLinearLayout();
+            LinearLayout original = new LinearLayout(ctx);
             LinearLayout buttons = new LinearLayout(ctx);
+            LinearLayout newMSG_Header = new LinearLayout(ctx);
+            LinearLayout newMSG = new LinearLayout(ctx);
+            
             var content = message.getContent();
 
-            var copyIcon = ContextCompat.getDrawable(ctx, R.e.ic_copy_24dp).mutate();
-            var deleteIcon = ContextCompat.getDrawable(ctx, R.e.ic_delete_24dp).mutate();
+            if (!content.isEmpty()) {
+                TextView originalHeader = new TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header);
+                originalHeader.setText("Original message");
+                originalHeader.setTypeface(ResourcesCompat.getFont(ctx, Constants.Fonts.whitney_semibold));
+                originalHeader.setPadding(0, DimenUtils.dpToPx(12), 0, 0);
+                layout.addView(originalHeader);
+                
+                TextView originalMessage = new TextView(ctx);
+                originalMessage.setText(MDUtils.renderCodeBlock(ctx, new SpannableStringBuilder(), null, content));
+                originalMessage.setTextIsSelectable(true);
+                layout.addView(originalMessage);
+            }
 
-            var copyButton = new Button(ctx);
+            var copyIcon = ContextCompat.getDrawable(ctx, R.e.ic_copy_24dp).mutate();
+            Button copyButton = new Button(ctx);
             LinearLayout.LayoutParams copyButtonParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
             copyButtonParams.rightMargin = DimenUtils.dpToPx(3);
             copyButtonParams.weight = 1;
             copyButton.setLayoutParams(copyButtonParams);
             copyButton.setText("Copy Content");
             copyButton.setCompoundDrawablesWithIntrinsicBounds(copyIcon, null, null, null);
+            copyButton.setEnabled(!content.isEmpty());
             copyButton.setOnClickListener(v -> {
                 Utils.setClipboard("Copy Content", content);
                 Utils.showToast(ctx, "Copied content to clipboard");
+                logger.info("Copied content to clipboard");
             });
-
-            var deleteButton = new DangerButton(ctx);
+            
+            var deleteIcon = ContextCompat.getDrawable(ctx, R.e.ic_delete_24dp).mutate();
+            DangerButton deleteButton = new DangerButton(ctx);
             LinearLayout.LayoutParams deleteButtonParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
             deleteButtonParams.leftMargin = DimenUtils.dpToPx(3);
             deleteButtonParams.weight = 1;
             deleteButton.setLayoutParams(deleteButtonParams);
-            deleteButton.setText("Delete Message");
+            deleteButton.setText("Delete Message locally");
             deleteButton.setCompoundDrawablesWithIntrinsicBounds(deleteIcon, null, null, null);
             deleteButton.setOnClickListener(v -> {
                 getActivity().onBackPressed();
@@ -85,6 +113,7 @@ public class EditMessages extends Plugin {
                     StoreStream.getMessages().handleMessageDelete(new ModelMessageDelete(message.getChannelId(), message.getId()));
                 }
                 Utils.showToast(ctx, "Message deleted");
+                logger.info("Message deleted");
             });
 
             buttons.setPadding(0, 0, 0, DimenUtils.dpToPx(12));
@@ -92,6 +121,51 @@ public class EditMessages extends Plugin {
             buttons.addView(deleteButton);
 
             layout.addView(buttons);
+
+            var newMessageHeader = new TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header);
+            newMessageHeader.setText("Edit message");
+            newMessageHeader.setTypeface(ResourcesCompat.getFont(ctx, Constants.Fonts.whitney_semibold));
+            newMessageHeader.setPadding(0, DimenUtils.dpToPx(12), 0, 0);
+            newMSG_Header.addView(newMessageHeader);
+            layout.addView(newMSG_Header);
+
+            Button saveButton = new Button(ctx);
+            EditText newMessage = new EditText(ctx);
+            LinearLayout.LayoutParams newMessageParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            newMessageParams.weight = 1;
+            newMessage.setLayoutParams(newMessageParams);
+            newMessage.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+            newMessage.setText(!content.isEmpty() ? content : "");
+            newMessage.setTextColor(Color.WHITE);
+            newMessage.addTextChangedListener(new TextWatcher(){
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    saveButton.setEnabled(!s.toString().isEmpty());
+                }
+            });
+            newMSG.addView(newMessage);
+            layout.addView(newMSG);
+
+            LinearLayout.LayoutParams saveButtonParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            saveButtonParams.setLayoutDirection(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
+            saveButton.setLayoutParams(saveButtonParams);
+            saveButton.setText("Update");
+            // var saveIcon = ContextCompat.getDrawable(ctx, R.e.icon_save).mutate();
+            // saveButton.setCompoundDrawablesWithIntrinsicBounds(saveIcon, null, null, null);
+            saveButton.setEnabled(!newMessage.getText().toString().isEmpty());
+            saveButton.setOnClickListener(v -> {
+                Utils.showToast(ctx, newMessage.getText().toString());
+                logger.info("Updated Message");
+            });
+            layout.addView(saveButton);
         }
     }
     
