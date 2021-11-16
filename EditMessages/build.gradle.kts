0fc2ca06de1930728cc8f6186f88cd33a00653d6
HypedDomi <domi@bambus.me>
2021-11-16 17:03:47 +0100

    Added EditPage

--- EditMessages/build.gradle.kts
+++ EditMessages/build.gradle.kts
@@ -1,2 +1,11 @@
-version = "0.1"
+version = "0.2"
 description = "Edit messages locally until next restart"
+
+aliucord.changelog.set(
+    """
+    New {added marginTop}
+    ======================
+    * Added EditPage
+    * Added Option to copy or delete message
+    """.trimIndent()
+)
--- EditMessages/src/main/java/me/bambus/plugins/EditMessages.java
+++ EditMessages/src/main/java/me/bambus/plugins/EditMessages.java
@@ -3,28 +3,98 @@ package me.bambus.plugins;
 import android.content.Context;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.TextView;
 import androidx.core.widget.NestedScrollView;
 import androidx.core.content.ContextCompat;
 import com.lytefast.flexinput.R;
-
 import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.entities.Plugin;
+import com.aliucord.Logger;
+import com.aliucord.fragments.SettingsPage;
+import com.aliucord.views.Button;
+import com.aliucord.views.DangerButton;
+import com.aliucord.Utils;
+import com.aliucord.utils.DimenUtils;
 import com.aliucord.PluginManager;
 import com.aliucord.api.CommandsAPI;
-import com.aliucord.Utils;
 import com.discord.api.commands.ApplicationCommandType;
-
 import com.aliucord.patcher.Hook;
 import com.aliucord.CollectionUtils;
 import com.discord.widgets.chat.list.actions.WidgetChatListActions;
 import com.discord.utilities.color.ColorCompat;
+import com.discord.stores.StoreStream;
+import com.discord.models.domain.ModelMessageDelete;
+import com.discord.models.message.Message;
+import com.discord.models.user.CoreUser;
 
 import java.util.Collections;
 
 @SuppressWarnings("unused")
 @AliucordPlugin
 public class EditMessages extends Plugin {
+    public Logger logger = new Logger("EditMessages");
+
+    class EditPage extends SettingsPage {
+        private Message message;
+        public EditPage(Message message) {
+            this.message = message;
+        }
+    
+        @Override
+        public void onViewBound(View view) {
+            super.onViewBound(view);
+            setActionBarTitle("EditMessage");
+            setActionBarSubtitle(new CoreUser(message.getAuthor()).getUsername());
+            
+            var ctx = view.getContext();
+            LinearLayout layout = getLinearLayout();
+            LinearLayout buttons = new LinearLayout(ctx);
+            var content = message.getContent();
+
+            var copyIcon = ContextCompat.getDrawable(ctx, R.e.ic_copy_24dp).mutate();
+            var deleteIcon = ContextCompat.getDrawable(ctx, R.e.ic_delete_24dp).mutate();
+
+            var copyButton = new Button(ctx);
+            LinearLayout.LayoutParams copyButtonParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            copyButtonParams.rightMargin = DimenUtils.dpToPx(3);
+            copyButtonParams.weight = 1;
+            copyButton.setLayoutParams(copyButtonParams);
+            copyButton.setText("Copy Content");
+            copyButton.setCompoundDrawablesWithIntrinsicBounds(copyIcon, null, null, null);
+            copyButton.setOnClickListener(v -> {
+                Utils.setClipboard("Copy Content", content);
+                Utils.showToast(ctx, "Copied content to clipboard");
+            });
+
+            var deleteButton = new DangerButton(ctx);
+            LinearLayout.LayoutParams deleteButtonParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            deleteButtonParams.leftMargin = DimenUtils.dpToPx(3);
+            deleteButtonParams.weight = 1;
+            deleteButton.setLayoutParams(deleteButtonParams);
+            deleteButton.setText("Delete Message");
+            deleteButton.setCompoundDrawablesWithIntrinsicBounds(deleteIcon, null, null, null);
+            deleteButton.setOnClickListener(v -> {
+                getActivity().onBackPressed();
+                if (PluginManager.isPluginEnabled("MessageLogger")) {
+                    logger.info("Due to how this plugin works, MessageLogger needs to be disabled");
+                    PluginManager.disablePlugin("MessageLogger");
+                    StoreStream.getMessages().handleMessageDelete(new ModelMessageDelete(message.getChannelId(), message.getId()));
+                    PluginManager.enablePlugin("MessageLogger");
+                } else {
+                    StoreStream.getMessages().handleMessageDelete(new ModelMessageDelete(message.getChannelId(), message.getId()));
+                }
+                Utils.showToast(ctx, "Message deleted");
+            });
+
+            buttons.setPadding(0, 0, 0, DimenUtils.dpToPx(12));
+            buttons.addView(copyButton);
+            buttons.addView(deleteButton);
+
+            layout.addView(buttons);
+        }
+    }
+    
     @Override
     public void start(Context context) {
         commands.registerCommand("editmessages", "Displays information about EditMessages",
@@ -44,6 +114,7 @@ public class EditMessages extends Plugin {
             }
             return new CommandsAPI.CommandResult(output, null, ctx.getBoolOrDefault("send", false));
         });
+        logger.info("Command registered");
 
         var id = View.generateViewId();
         patcher.patch(WidgetChatListActions.class, "configureUI", new Class<?>[]{ WidgetChatListActions.Model.class }, new Hook(
@@ -62,11 +133,17 @@ public class EditMessages extends Plugin {
                 editDrawable.setTint(ColorCompat.getThemedColor(ctx, R.b.colorInteractiveNormal));
                 view.setCompoundDrawablesRelativeWithIntrinsicBounds(editDrawable, null, null, null);
                 view.setOnClickListener(e -> {
-                    Utils.showToast("Clicked Button", false);
-                    _this.dismiss();
+                    Utils.openPageWithProxy(e.getContext(), new EditPage(msg));
                 });
-                layout.addView(view, 9);
+
+                for (int i = 0; i < layout.getChildCount(); i++) {
+                    if (layout.getChildAt(i).getId() == Utils.getResId("dialog_chat_actions_delete", "id")) {
+                        layout.addView(view, i+1);
+                        break;
+                    }
+                }
             }));
+        logger.info("MessageContextMenu patched");
     }
 
     @Override
